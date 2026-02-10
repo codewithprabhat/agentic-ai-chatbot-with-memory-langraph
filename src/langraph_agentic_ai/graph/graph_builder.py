df23e1375feaf2be32f859e9b366f2c772df58db
prabhatkumarjena007 <prabhat.jena@tech9.com>
2026-02-10 21:06:33 +0530

    added agent with tool integration for web search

--- src/langraph_agentic_ai/graph/graph_builder.py
+++ src/langraph_agentic_ai/graph/graph_builder.py
@@ -1,6 +1,10 @@
 from langgraph.graph import StateGraph, START, END
 from src.langraph_agentic_ai.state.state import State
 from src.langraph_agentic_ai.nodes.basic_chatbot_node import BasicChatbotNode
+from src.langraph_agentic_ai.nodes.chatbot_with_tool_node import ChatbotWithToolNode
+from src.langraph_agentic_ai.tools.search_tool import get_tools, create_search_tool
+from langgraph.prebuilt import tools_condition
+
 
 
 class GraphBuilder:
@@ -22,11 +26,38 @@ class GraphBuilder:
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot", END)
 
+    def chatbot_with_tools_graph(self):
+        """
+            Builds an advanced chatbot graph with tool integration.
+            This method creates a chatbot graph that includes both a chatbot node 
+            and a tool node. It defines tools, initializes the chatbot with tool 
+            capabilities, and sets up conditional and direct edges between nodes. 
+            The chatbot node is set as the entry point.
+        """
+
+        tools = get_tools()
+        tool_node = create_search_tool(tools)
+
+        self.chatbot_with_tool_node = ChatbotWithToolNode(self.model)
+        chatbot_node = self.chatbot_with_tool_node.create_chatbot(tools)
+
+
+        self.graph_builder.add_node("chatbot", chatbot_node)
+        self.graph_builder.add_node("tools", tool_node)
+
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_conditional_edges("chatbot", tools_condition)
+        self.graph_builder.add_edge("tools", "chatbot")
+        self.graph_builder.add_edge("chatbot", END)
+       
+
     def setup_graph(self):
         """
         Sets up the graph for the selected use case.
         """
         if self.use_case == "Basic Chatbot":
             self.basic_chatbot_graph()
+        elif self.use_case == "Chatbot With Web":
+            self.chatbot_with_tools_graph()
         
         return self.graph_builder.compile()
--- src/langraph_agentic_ai/ui/streamlit_ui/load_ui.py
+++ src/langraph_agentic_ai/ui/streamlit_ui/load_ui.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.langraph_agentic_ai.ui.ui_config_file import Config
+import os
 
 class LoadStreamlitUI:
     def __init__(self):
@@ -29,7 +30,14 @@ class LoadStreamlitUI:
                     st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
 
             ## Usecase selection
-            self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)    
+            self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
+            if self.user_controls["selected_usecase"] == 'Chatbot With Web':
+                os.environ["TAVILY_API_KEY"]=self.user_controls["TAVILY_API_KEY"]=st.session_state["TAVILY_API_KEY"]=st.text_input("TAVILY API KEY",type="password")
+
+                # Validate API key
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("⚠️ Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
+
 
         return self.user_controls
 
